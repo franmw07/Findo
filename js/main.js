1fd22557b2430b1b9c3647dfcad0def5e79d479a
Francisco Martinez <fran@educareality.com>
2022-01-19 10:25:37 +0100

    desarrollo responsibe

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const type = new Typed('.typed', {
+    strings: ['cerca de ti'],
+    typeSpeed: 75,
+    startDelay: 1200,
+    backSpeed: 75,
+    backDelay: 3000,
+    loop: true,
+    loopCount: false,
+});
